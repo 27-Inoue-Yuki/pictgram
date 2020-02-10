aa73dfadb49a96d6681ec606c648436666eef5d5
27-Inoue-Yuki <t.shock.manu001@gmail.com>
2020-02-10 12:18:22 +0000

    Rails05提出

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,8 @@ class SessionsController < ApplicationController
   end
   
   def create
-    user = User.find_by(email: params[:session][:email])
-    if user && user.authenticate(params[:session][:password])
+    user = User.find_by(user_params)
+    if user && user.authenticate(user_params)
       log_in user
       redirect_to root_path, success:'ログインに成功しました'
     else
@@ -13,9 +13,19 @@ class SessionsController < ApplicationController
     end
   end
   
+  def destroy
+    log_out
+    redirect_to root_url, info:'ログアウトしました'
+  end
+  
   private
   def log_in(user)
     session[:user_id] = user.id
+  end
+  
+  def log_out
+    session.delete(:user_id)
+    @cureent_user = nil
   end
 end
 
