ec7a21adc8dbc91c564b9dd68aebf7cb7cccbedb
27-Inoue-Yuki <t.shock.manu001@gmail.com>
2020-02-24 13:02:17 +0000

    commentページの保存場所作成

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,8 +1,20 @@
 class CommentsController < ApplicationController
-  def comment
+  def new
+    @comment = Comment.new
   end
   
-  def helo
+  def create
+    @comment = Comment.new(comment_params)
+    
+    if @comment.save
+      redirect_to topics_path,success:"投稿に成功しました"
+    else
+      flash.now[:danger] ='投稿に失敗しました'
+      render:new
+    end
   end
-  
+  private
+  def comment_params
+    params.require(:comment).permit(:comment)
+  end
 end
