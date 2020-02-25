25751768c1efa64c9a7cc3a64ae298e0885caf1d
27-Inoue-Yuki <t.shock.manu001@gmail.com>
2020-02-25 10:22:40 +0000

    models/micropost.rbのインデント修正、引数周りは解決済み？

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,6 +1,6 @@
 class Micropost < ApplicationRecord
-  has_many :likes,dependent: :destroy
-  has_many :iine_users,through: :likes, source: :user
+  has_many :likes, dependent: :destroy
+  has_many :iine_users, through: :likes, source: :user
   
   def iine(user)
     likes.create(user_id: user.id)
