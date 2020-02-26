e22cb7e6f6617cd14404b5890e8b8ea3d3679091
27-Inoue-Yuki <t.shock.manu001@gmail.com>
2020-02-26 06:49:18 +0000

    controllers/likes_controller.rbのインデント調整

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,12 +2,11 @@ class LikesController < ApplicationController
   before_action :logged_in_user
   def create
     @micropost = Micropost.find(params[:micropost_id])
-    unless @micropost.iine?(current_user)
-    respond_to do | format|
-      format.html{ redirect_to request.referrer || root_url }
-      format.js
-    end
-      
+    unless @micropost.iine?(current_user) then
+      respond_to do | format|
+        format.html{ redirect_to request.referrer || root_url }
+        format.js
+      end
     end
     @micropost.iine(current_user)
   end
