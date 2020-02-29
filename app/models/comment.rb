1545800bba87f5dd2f4f1d688bed3c6fce83120c
27-Inoue-Yuki <t.shock.manu001@gmail.com>
2020-02-29 10:27:51 +0000

    models/comment条件の追記

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
 class Comment < ApplicationRecord
-    belongs_to:topic
+    belongs_to :topic
+    validates :topic_id, presence:true
+    validates :description, presence:true
 end
