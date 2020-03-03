cf7cf49b852cfb4966a095bd88beefbe5d3ce836
baby_gang <b.gang.k@gmail.com>
2020-03-04 04:31:45 +0900

    コメント周りのmodelを更新

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_24_104826) do
+ActiveRecord::Schema.define(version: 2020_03_03_191813) do
 
   create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "comment"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "topic_id"
   end
 
   create_table "favorites", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
