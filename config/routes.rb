91afb8a049bfa823df068934ef452107c17263b5
baby_gang <b.gang.k@gmail.com>
2020-03-04 04:35:36 +0900

    コメント機能の画面周りを実装

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   
   get 'pages/comment'
   get 'pages/help'
-
+  get 'pages/comment/new'
   get  '/login', to: 'sessions#new'
   post  '/login', to: 'sessions#create'
   delete  '/logout', to: 'sessions#destroy'
